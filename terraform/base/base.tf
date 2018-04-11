29162f55089f9b11db0a77bb25f06b794594db43
rhysr <rhys@mega-city-one.co.uk>
2018-04-11 19:46:37 +0100

    Add variables for region and AMI

--- terraform/base/base.tf
+++ terraform/base/base.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region  = "us-east-2"
+  region  = "${var.region}"
   version = "~> 1.14"
 }
 
 resource "aws_instance" "base" {
   # ubuntu-core/images/hvm-ssd/ubuntu-core-16-amd64-server-20161108
-  ami           = "ami-ad4812c8"
+  ami           = "${var.ami}"
   instance_type = "t2.micro"
 }
 
---
+++ terraform/base/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type = "string"
+  description = "The AWS region."
+}
+
+variable "ami" {
+  type = "string"
+  description = "The AMI to use."
+}
